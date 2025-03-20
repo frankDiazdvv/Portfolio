8afa391c37b8ba15c94441acea53f7233109cd2b
Frank Diaz <158342906+frankDiazdvv@users.noreply.github.com>
2025-03-20 12:25:36 -0400

    Delete Portfolio directory

--- Portfolio/script.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".side-menu-icon");
-
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
